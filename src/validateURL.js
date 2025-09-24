f3fa476eb51d7430ae6160dea43ec382f67728d1
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2025-09-24 12:36:35 +0500

    fix url validation func

--- src/validateURL.js
+++ src/validateURL.js
@@ -3,5 +3,8 @@ import * as yup from 'yup'
 export default (url, state) => {
   const schema = yup.string().required().url().notOneOf(state.feeds)
   return schema.validate(url)
-    .then(result => state.feeds.push(result))
+    .then((validatedUrl) => {
+      state.feeds.push(validatedUrl)
+      return validatedUrl
+    })
 }

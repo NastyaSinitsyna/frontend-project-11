000fef9bf49ce55b83df85ccb5467ddeebe00b42
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2025-11-10 10:21:57 +0300

    add prewatchButton with modal opened by click

--- src/view.js
+++ src/view.js
@@ -1,6 +1,7 @@
 import 'bootstrap'
 // import i18n from 'i18next'
 import './style.css'
+// import onChange from 'on-change'
 
 export const renderErrors = (state, i18n) => {
   const input = document.querySelector('#url-input')
@@ -42,6 +43,71 @@ export const renderFeedsAndPosts = (state, i18n) => {
   state.posts.forEach((post) => {
     const postTitle = document.createElement('h3')
     postTitle.textContent = post.title
-    postsContainer.appendChild(postTitle)
+    postTitle.classList.add('fw-bold')
+    const prewatchButton = document.createElement('button')
+    prewatchButton.classList.add('btn', 'btn-outline-primary')
+    prewatchButton.dataset.bsToggle = 'modal'
+    prewatchButton.dataset.bsTarget = '#modal'
+    prewatchButton.textContent = i18n.t('view.prewatchButton')
+    prewatchButton.addEventListener('click', () => {
+      openModal(i18n)
+    })
+    postTitle.append(prewatchButton)
+    postsContainer.append(postTitle)
   })
+}
+
+export const openModal = (i18n) => {
+  const modal = document.createElement('div')
+  modal.classList.add('modal', 'fade')
+  modal.setAttribute('id', 'modal')
+  modal.tabindex = -1
+  modal.setAttribute('aria-labelledby', 'modalLabel')
+  modal.setAttribute('aria-hidden', 'true')
+  modal.setAttribute('role', 'dialog')
+
+  const modalDialog = document.createElement('div')
+  modalDialog.classList.add('modal-dialog')
+  modal.append(modalDialog)
+
+  const modalContent = document.createElement('div')
+  modalDialog.classList.add('modal-content')
+  modalDialog.append(modalContent)
+
+  const modalHeader = document.createElement('div')
+  modalDialog.classList.add('modal-header')
+  modalContent.append(modalHeader)
+  const modalTitle = document.createElement('h5')
+  modalTitle.classList.add('modal-title')
+  modalTitle.setAttribute('id', 'modaLabel')
+  modalTitle.textContent = 'Modal Title Check'
+  const closeButton = document.createElement('button')
+  closeButton.setAttribute('type', 'button')
+  closeButton.classList.add('btn-close')
+  closeButton.dataset.bsDismiss = 'modal'
+  closeButton.setAttribute('aria-label', 'Close')
+  modalHeader.append(modalTitle, closeButton)
+
+  const modalBody = document.createElement('div')
+  modalBody.classList.add('modal-body', 'text-break')
+  modalBody.textContent = 'modalBody Text Check'
+  modalContent.append(modalBody)
+
+  const modalFooter = document.createElement('div')
+  modalDialog.classList.add('modal-footer')
+  modalContent.append(modalFooter)
+
+  const readButton = document.createElement('button')
+  readButton.setAttribute('type', 'button')
+  readButton.classList.add('btn-primary')
+  readButton.textContent = i18n.t('view.readButton')
+
+  const closeFooterButton = document.createElement('button')
+  closeFooterButton.setAttribute('type', 'button')
+  closeFooterButton.classList.add('btn-secondary')
+  closeFooterButton.dataset.bsDismiss = 'modal'
+  closeFooterButton.textContent = i18n.t('view.closeButton')
+  modalFooter.append(readButton, closeFooterButton)
+
+  document.body.append(modal)
 }

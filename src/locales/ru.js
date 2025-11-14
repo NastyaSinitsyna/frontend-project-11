8bcf4cf6f2aa0bea3c9331095ff9a15cabfd55e7
NastyaSinitsyna <ren2507@yandex.ru>
2025-11-14 10:29:53 +0300

    add interface for success url load and invalid rss check

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -3,15 +3,15 @@ export default {
     errors: {
       duplicate: 'RSS уже существует',
       invalidUrl: 'Ссылка должна быть валидным URL',
+      invalidRss: 'Ресурс не содержит валидный RSS',
       networkError: 'Ошибка сети',
       unknown: 'Неизвестная ошибка',
     },
+    success: 'RSS успешно загружен',
     view: {
       feedsHeader: 'Фиды',
       postsHeader: 'Посты',
       prewatchButton: 'Просмотр',
-      closeButton: 'Закрыть',
-      readButton: 'Читать полностью',
     },
   },
 }

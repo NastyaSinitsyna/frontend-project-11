9ba47ffb2351f1b34bd57c35230fcdd2fd8684ef
NastyaSinitsyna <ren2507@yandex.ru>
2025-11-28 10:40:57 +0300

    fix parse function for full parsing and dom transfrom data

--- src/getPosts.js
+++ src/getPosts.js
@@ -2,42 +2,35 @@ import axios from 'axios'
 import parse from './parse.js'
 import _ from 'lodash'
 
-const getUrlContents = (feedUrl) => {
+const getPosts = (feedUrl, state) => {
   return axios.get('https://allorigins.hexlet.app/get', {
     params: {
       disableCache: true,
       url: feedUrl,
     },
   })
-    .then(response => parse(response.data.contents, 'text/xml'))
-}
-
-const getPosts = (feedUrl, state) => getUrlContents(feedUrl)
-  .then((contents) => {
-    const currentFeed = {
-      feedUrl: feedUrl,
-      feedId: _.uniqueId(),
-      title: contents.querySelector('channel > title').textContent,
-      description: contents.querySelector('channel > description').textContent,
-    }
-    if (!state.feeds.find(stateFeed => stateFeed.feedUrl === currentFeed.feedUrl)) {
-      state.feeds.push(currentFeed)
-    }
-
-    const posts = [...contents.querySelectorAll('item')].map(post => ({
-      title: post.querySelector('title').textContent,
-      link: post.querySelector('link').textContent,
-      description: post.querySelector('description').textContent,
-      feedId: currentFeed.feedId,
-      postId: post.querySelector('link').textContent.replace(/\W+/g, '_'),
-    }))
-    posts.forEach((post) => {
-      if (!state.posts.find(statePost => statePost.postId === post.postId)) {
-        state.posts.push(post)
+    .then((response) => {
+      const { feedData, postsData } = parse(response.data.contents, 'text/xml')
+      const currentFeed = {
+        ...feedData,
+        feedUrl: feedUrl,
+        feedId: _.uniqueId(),
       }
+      if (!state.feeds.find(stateFeed => stateFeed.feedUrl === currentFeed.feedUrl)) {
+        state.feeds.push(currentFeed)
+      }
+
+      const posts = postsData.map(post => ({
+        ...post,
+        feedId: currentFeed.feedId,
+      }))
+      posts.forEach((post) => {
+        if (!state.posts.find(statePost => statePost.postId === post.postId)) {
+          state.posts.push(post)
+        }
+      })
+      return state
     })
-    console.log(state)
-    return state
-  })
+}
 
 export default getPosts
--- src/parse.js
+++ src/parse.js
@@ -7,5 +7,15 @@ export default (responseContent, format) => {
     err.name = 'RssError'
     throw err
   }
-  return contents
+  const feedData = {
+    title: contents.querySelector('channel > title').textContent,
+    description: contents.querySelector('channel > description').textContent,
+  }
+  const postsData = [...contents.querySelectorAll('item')].map(post => ({
+    title: post.querySelector('title').textContent,
+    link: post.querySelector('link').textContent,
+    description: post.querySelector('description').textContent,
+    postId: post.querySelector('link').textContent.replace(/\W+/g, '_'),
+  }))
+  return { feedData, postsData }
 }
